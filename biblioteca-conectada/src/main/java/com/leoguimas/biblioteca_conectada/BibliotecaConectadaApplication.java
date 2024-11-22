dc15513bb00cf3d66293784def1385ff807c897e
LeoGuimas <gleo.dev0@gmail.com>
2024-11-22 18:20:52 -0300

    Initialize Spring Boot project with Maven configuration, application class, and test setup

---
+++ biblioteca-conectada/src/main/java/com/leoguimas/biblioteca_conectada/BibliotecaConectadaApplication.java
@@ -0,0 +1,13 @@
+package com.leoguimas.biblioteca_conectada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaConectadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaConectadaApplication.class, args);
+	}
+
+}
---
+++ biblioteca-conectada/src/test/java/com/leoguimas/biblioteca_conectada/BibliotecaConectadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoguimas.biblioteca_conectada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaConectadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
